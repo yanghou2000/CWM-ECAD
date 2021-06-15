55c55f755e201d1ae77231097043499bf43c2e20
Cloud User <centos@ip-172-31-15-220.eu-west-2.compute.internal>
2021-06-15 15:26:52 +0000

    not finished ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name:Yang Hou
+// Date: 2021/6/15
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -15,4 +15,42 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module LED (
+    //Todo: add ports 
+    input rst,
+    input clk,
+    input button,
+    output [2:0] color
+    );  
+
+    //Todo: add registers and wires, if needed
+    output reg [2:0] color;
+
+    //Todo: add user logic
+    always @(posedge clk)
+    begin
+         if (rst)
+            #1 color <= 3'b001;
+         else
+             begin
+             if (button == 0)
+                color <= color;
+             else
+                 if (color == 3'b001)
+                   #1 color <= 3'b010;
+                 if (color == 3'b010)
+                   #1 color <= 3'b011;
+                 if (color == 3'b011)
+                   #1 color <= 3'b100;
+                 if (color == 3'b100)
+                   #1 color <= 3'b101;
+                 if (color == 3'b101)
+                   #1 color <= 3'b110;
+                 if (color == 3'b110)
+                   #1 color <= 3'b001;
+             end
+    end  
+endmodule                      
+             
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,91 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name:Yang Hou
+// Date: 2021/6/15
 //
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+
+//Todo: Parameters
+parameter CLK_PERIOD = 10;
+
+//Todo: Regitsers and wires
+    reg rst;
+    reg button;
+    reg clk;
+    reg err;
+    reg [2:0] color_pre;
+    wire [2:0] color;
+
+//Todo: Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+//Todo: Botton generation
+    initial
+    begin
+       button = 1'b0;
+       forever
+         #(CLK_PERIOD/2) button=~button;
+    end
+//Todo: User logic
+    initial 
+    begin
+    rst = 1;
+    button = 0;
+    err = 0;
+       forever
+       #(CLK_PERIOD)
+       begin
+            if ((rst == 1) && (color != 3'b001))
+               begin
+                    $display("TEST FAILED");
+                    err = 1;
+               end
+            
+            rst = 0;	
+            color_pre = color;
+
+            if((button == 0) && (color_pre != color))
+			begin
+			     $display("TEST FAILED");
+			     err = 1;
+			end
+
+            button = 1;
+	    color_pre = color;
+
+            if((button == 1) && (color != color_pre +1))
+                        begin
+				$display("TEST FAILED");
+				err = 1;
+			end
+       end
+    end
+
+//Todo: Finish test, check for success
+	initial begin
+	  #100
+	  if (err == 0)
+		$display("***TEST PASSED! :) ***");
+   	  $finish;
+	end
+
+//Todo: Instantiate counter module
+	     LED top(
+		.clk (clk),
+		.rst (rst),
+ 		.button (button),
+		.color (color));
+ endmodule 
+
+    
