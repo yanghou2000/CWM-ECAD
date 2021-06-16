158de36a25236da94f8a5d84a3402a7c0c1f4555
Cloud User <centos@ip-172-31-15-220.eu-west-2.compute.internal>
2021-06-16 10:55:32 +0000

    complete version

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name:Yang HOU
+// Date: 2021/6/16
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -12,4 +12,34 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module ACS (
+	//add ports
+	input clk,
+	input [4:0] temp,
+	
+	output heating,
+	output cooling
+	);
+
+	//add registers and wires if required
+	reg [1:0] state;
+
+	//heating on = 10, cooling on = 01, idle = 00        
+	assign heating = state[1];
+	assign cooling = state[0];
+
+	//add logic
+	always @(posedge clk)
+	begin
+		case(state)
+                2'b10 : state = temp<20 ? 2'b10 : 2'b00;
+                2'b01 : state = temp>20 ? 2'b01 : 2'b00;
+		2'b00 : state = temp<=18 ? 2'b10 : temp>=22 ? 2'b01 : 2'b00;
+
+		default: state = 2'b00;
+		endcase
+	end
+endmodule
+        
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,76 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name:Yang Hou
+// Date: 2021/6/16
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+
+//Todo: Parameters
+parameter CLK_PERIOD = 10;
+
+//Todo: Regitsers and wires
+    reg clk;
+    reg err;
+    reg [4:0] temp;
+    wire heating;
+    wire cooling;
+
+//Todo: Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+//Todo: User logic
+    initial
+    begin
+	err = 0;
+	temp = 5'd1;
+
+	forever
+	#(CLK_PERIOD + 20)
+	begin
+		if((temp<=18) && (heating!=1))
+			begin			
+			$display("heating fail");
+			err = 1;
+			end
+		if((temp>=22)&&(cooling!=1))
+			begin
+			$display("cooling fail");
+			err = 1;
+			end
+		if((18<temp<22)&&(heating!=0)&&(cooling!=0))
+			begin
+			$display("maintaing idle fail");
+			err = 1;
+			end
+		temp = temp + 1;
+	end
+    end
+
+//Todo: Finish test, check for success
+    initial begin
+      #100
+      if (err == 0)
+		$display("***TEST PASSED! :) ***");
+      $finish;
+    end
+
+//Todo: Instantiate counter module
+    ACS top(
+            .clk (clk),
+            .temp (temp),
+            .heating (heating),
+            .cooling (cooling));
+
+
+endmodule
