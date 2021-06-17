aab738b893aaf0f09475888d84eb016f54d48a0a
Cloud User <centos@ip-172-31-15-220.eu-west-2.compute.internal>
2021-06-17 11:25:20 +0000

    completed version

--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name:Yang Hou
+// Date: 2021/6/17
 //
 //
 //  Description: In this exercise, you need to design a memory with 8 entries, 
@@ -14,4 +14,25 @@
 //           rgb [23:0]
 //
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module RGB(
+	// ports
+	input clk,
+	input [2:0] color,
+	input enable,
+
+	output [23:0] rgb
+	);
+
+  
+mytruebram RGB (
+  .clka(clka),    // input wire clka
+  .ena(ena),      // input wire ena
+  .wea(wea),      // input wire [0 : 0] wea
+  .addra(addra),  // input wire [2 : 0] addra
+  .dina(dina),    // input wire [23 : 0] dina
+  .douta(douta)  // output wire [23 : 0] douta
+);
+
+endmodule
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -1,9 +1,71 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name:Yang Hou
+// Date: 2021/6/17
 //
 // Description: A testbench module to test Ex6 - RGB Colour Converter
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+
+//Todo: Parameters
+	parameter CLK_PERIOD = 10;
+ 	
+//Todo: Regitsers and wires
+	reg clk;
+	reg enable;
+	reg err;
+	
+	reg [2:0] color;
+	wire [23:0] rgb;
+	
+//Todo: Clock generation
+	initial
+		begin
+		    clk = 0;
+		    forever
+		      #(CLK_PERIOD/2) clk=~clk;//flip the clock signal every half period
+		end			
+//Todo: User logic
+	initial 
+	begin
+	err = 0;
+	enable = 0;
+	color = 3'b000;
+	#CLK_PERIOD
+	enable = 1;
+	forever begin
+	 #CLK_PERIOD
+		
+		if((color[0] != (&rgb[7:0])) || (color[1] != (&rgb[15:8])) || (color[2] != (&rgb[23:16])))
+			begin
+			  $display("TEST FAILED bit conversion");
+			  err = 1;
+			end
+		
+		color = color + 3'b001;
+		end
+	end
+	
+//Todo: Finish test, check for success
+	initial begin
+	  #500
+	  if (err == 0)
+		$display("***TEST PASSED! :) ***");
+   	  $finish;
+	end
+
+
+
+
+//Todo:Instantiate counter module
+	 RGB top(
+		.clk (clk),
+		.enable (enable),
+		.color (color),
+		.rgb (rgb)
+		);
+endmodule
